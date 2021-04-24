1afcf52054ec1d7d51d413c1213529b49c9c63d2
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-04-24 16:54:56 +0300

    me and nadeen works on date and time

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,55 +1,41 @@
 'use strict';
-
+//////////////////////////////////////////////////////////////////////////////////////////
 const feedbackForm = document.getElementById('feedbackForm');
 const feedbacks = document.getElementById('feedbacks');
 const textarea = document.getElementById('textarea');
-
-
-function Feed(feedback) {
+const textName = document.getElementById('name');
+//////////////////////////////////////////////////////////////////////////////////////////
+function Feed(feedback, name, date) {
     this.feedback = feedback;
-
+    this.name = name;
+    this.data = date;
     Feed.allFeeds.push(this);
     saveToLs();
 }
-
 Feed.allFeeds = [];
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
 function handleSubmit(event) {
     event.preventDefault();
-    console.log(event.target);
-
     const feedback = event.target.feedback.value;
-
-    new Feed(feedback);
-
+    const name = event.target.name.value;
+    console.log(name);
+    let b = new Feed(feedback, name, gettingFeedbackDate());
     renderFeedBack();
 }
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
 function saveToLs() {
-
     let arrStr = JSON.stringify(Feed.allFeeds);
-
     localStorage.setItem('feedSaved', arrStr);
-
 }
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
 function gettingFeedFormLs() {
     let data = localStorage.getItem('feedSaved');
-
-    console.log(data);
-    let order = JSON.parse(data);
-    console.log(order);
-    if (order !== null) {
-        Feed.allFeeds = order;
+    if (JSON.parse(data) !== null) {
+        Feed.allFeeds = JSON.parse(data);
     }
     renderFeedBack();
-
 }
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
 function renderFeedBack() {
     feedbacks.textContent = '';
     textarea.value = '';
@@ -59,16 +45,27 @@ function renderFeedBack() {
         count++;
         feedli.textContent = `${Feed.allFeeds[i].feedback} `;
         feedbacks.appendChild(feedli);
-
         if (count == 5) {
             break;
         }
-
-
     }
 }
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
+function gettingFeedbackDate() {
+    let d = new Date();
+    // const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    // const monthNames = ["January", "February", "March", "April", "May", "June",  "July", "August", "September", "October", "November", "December"];
+    // let day = days[d.getDay()];
+    // let month = monthNames[d.getMonth() + 1] ;
+    // console.log(month);
+    // console.log( day);
+    // console.log(typeof d);
+    let time = d.toLocaleTimeString();
+    // let time = d.toTimeString();
+    let date = d.toDateString();
+    date += " " + time;
+    return date;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
 feedbackForm.addEventListener('submit', handleSubmit);
-
 gettingFeedFormLs();
