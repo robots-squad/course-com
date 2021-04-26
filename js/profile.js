813a7cc11a1114730ca8289b50f20d40c18f9b49
latifaghassan <lalooshamayra@hotmail.com>
2021-04-26 14:14:00 +0300

    added css profile

--- js/profile.js
+++ js/profile.js
@@ -6,36 +6,45 @@ let purRender = document.getElementById('purchases');
 let favRender = document.getElementById('favourites');
 
 function renderCourses(){
-  
-    for(let i = 0; i < Cards.allCourses.length; i++){
-      if (Cards.allCourses[i].buy){
+  let vr=null;
+    for(let i = 0; i <50; i++){
+      vr=document.createElement('h1');
+   favRender.appendChild(vr);
+   vr.textContent=5;
+ }   
 
-      image = document.createElement('img');
-      purRender.appendChild(image);
-      image.setAttribute('src', Cards.allCourses[i].src)
-      
-      nameCourse = document.createElement('h3');
-      purRender.appendChild(nameCourse);
-      nameCourse.textContent = `${Cards.allCourses[i].name}`;
-    } else { 
-        nameCourse = document.createElement('h2');
-        purRender.appendChild(nameCourse);
-        nameCourse.textContent = "You haven't purchase any courses";
     }
 
-     if(Cards.allCourses[i].fav){
-        image = document.createElement('img');
-        favRender.appendChild(image);
-      image.setAttribute('src', Cards.allCourses[i].src)
+    renderCourses();
+      // if (Cards.allCourses[i].buy){
+
+    //   image = document.createElement('img');
+    //   purRender.appendChild(image);
+    //   image.setAttribute('src', Cards.allCourses[i].src)
       
-      nameCourse = document.createElement('h3');
-      favRender.appendChild(nameCourse);
-      nameCourse.textContent = `${Cards.allCourses[i].name}`; 
-     } else {
-        nameCourse = document.createElement('h2');
-        favRender.appendChild(nameCourse);
-        nameCourse.textContent = "You haven't choose any favourite courses";
-     }
+    //   nameCourse = document.createElement('p');
+    //   purRender.appendChild(nameCourse);
+    //   nameCourse.textContent = `${Cards.allCourses[i].name}`;
+    // } else { 
+    //     nameCourse = document.createElement('h2');
+    //     purRender.appendChild(nameCourse);
+    //     nameCourse.textContent = "You haven't purchase any courses";
+    // }
+
+    //  if(Cards.allCourses[i].fav){
+    //     image = document.createElement('img');
+    //     favRender.appendChild(image);
+    //   image.setAttribute('src', Cards.allCourses[i].src)
+      
+    //   nameCourse = document.createElement('p');
+    //   favRender.appendChild(nameCourse);
+    //   nameCourse.textContent = `${Cards.allCourses[i].name}`; 
+    //  } else {
+    //     nameCourse = document.createElement('h2');
+    //     favRender.appendChild(nameCourse);
+    //     nameCourse.textContent = "You haven't choose any favourite courses";
+    //  }
     
-    }   
-}
+    // }   
+// }
+
