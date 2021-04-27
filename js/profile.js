ee8305e35146e6b2d2f8e0ea81b53e9a3e136462
osama-alali <osama.alali89@yahoo.com>
2021-04-27 12:26:15 +0300

    working on profile page

--- js/profile.js
+++ js/profile.js
@@ -8,15 +8,22 @@ let favRender = document.getElementById('favourites');
 let hrefArr = ['https://youtu.be/ibPDmf83z_k', ]
 /****************** test / local storage get ************************** */
 
-let Cards = {
-  allCourses: [
-    {name: 'html',fav: true, buy: true, src: '../img/HTML.png'},
-    {name: 'css',fav: true, buy: true, src: '../img/CSS.png'}
+// let Cards = {
+//   allCourses: [
+//     {name: 'html',fav: true, buy: true, src: '../img/HTML.png'},
+//     {name: 'css',fav: true, buy: true, src: '../img/CSS.png'}
 
-  ]
+//   ]
+// }
+let data=[];
+function getLs() {
+   data = JSON.parse(localStorage.getItem('courses'));
 }
-
+getLs();
 /************************************************************************ */
+
+
+
 
 let emptyCourseBuy = document.createElement('h2');
     purRender.appendChild(emptyCourseBuy);
@@ -28,8 +35,8 @@ let emptyCoursefav = document.createElement('h2');
 
  function renderCourses(){
 
-    for(let i = 0; i <Cards.allCourses.length; i++){
-      if (Cards.allCourses[i].buy){
+    for(let i = 0; i <data.length; i++){
+      if (data[i].buy){
 
         emptyCourseBuy.style.display= 'none';
 
@@ -39,19 +46,19 @@ let emptyCoursefav = document.createElement('h2');
 
         image = document.createElement('img');
         href.appendChild(image);
-        image.setAttribute('src', Cards.allCourses[i].src);
+        image.setAttribute('src', data[i].src);
 
         purRender.appendChild(href);
         
       }
 
-     if(Cards.allCourses[i].fav){
+     if(data[i].fav){
 
         emptyCoursefav.style.display= 'none'
 
         image = document.createElement('img');
         favRender.appendChild(image);
-        image.setAttribute('src', Cards.allCourses[i].src)
+        image.setAttribute('src', data[i].src)
      } 
     
   }   
