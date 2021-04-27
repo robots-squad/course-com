b91c858e87115e17d684652557e45b78d488cb04
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-04-27 10:58:47 +0300

    add some changes

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let loginButton = document.getElementById('login');
+
+loginButton.addEventListener('click',function (){  
+
+  let x = prompt('enter your name')
+})
--- js/courses.js
+++ js/courses.js
@@ -339,38 +339,38 @@ for(let i=0; i< Cards.allCourses.length; i++){
   if(Cards.allCourses[i].allRrte != 0){  // to prevent dividing on 0
     avg = Cards.allCourses[i].allRrte / Cards.allCourses[i].count;
   }else{
-    avg=0;
+    avg = 0;
   }
+
   rateArr.push(avg);
 }
 
 // chart:
 function chart(){ 
 
-
   let ctx = document.getElementById('myChart').getContext('2d');
   let myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: namesArr,
-          datasets: [{
-              label: 'Rating',
-              data: rateArr,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.5)',
-                  'rgba(54, 162, 235, 0.5)',
-                  'rgba(255, 206, 86, 0.5)',
-                  'rgba(75, 192, 192, 0.5)',
-                  'rgba(153, 102, 255, 0.5)',
-                  'rgba(255, 159, 64, 0.5)',
-                  'rgba(255, 100, 200, 0.5)',
-                  'rgba(100, 159, 50, 0.5)',
-                  'rgba(53, 30, 250, 0.5)'
-              ],
-              borderWidth: 2
-          }]
-      }
-    })
+    type: 'bar',
+    data: {
+      labels: namesArr,
+      datasets: [{
+        label: 'Rating',
+        data: rateArr,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(54, 162, 235, 0.5)',
+          'rgba(255, 206, 86, 0.5)',
+          'rgba(75, 192, 192, 0.5)',
+          'rgba(153, 102, 255, 0.5)',
+          'rgba(255, 159, 64, 0.5)',
+          'rgba(255, 100, 200, 0.5)',
+          'rgba(100, 159, 50, 0.5)',
+          'rgba(53, 30, 250, 0.5)'
+        ],
+        borderWidth: 2
+      }]
+    }
+  })
 }
 
 chart();
